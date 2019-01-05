c16214834e1374bb918ba47254b757934b424de6
Kabiru Ahmed <you@example.com>
2019-01-05 21:28:38 +0100

    initial commit

---
+++ spring-config-client/src/main/java/com/hamsoft/springconfigclient/ApiController.java
@@ -0,0 +1,21 @@
+package com.hamsoft.springconfigclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ApiController {
+
+
+    @Value("${msg:Hello world - Config Server is not working..pelase check}")
+    private String msg;
+
+    @RequestMapping("/msg")
+    String getMsg() {
+        return this.msg;
+    }
+
+}
